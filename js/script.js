1fea647e1ee79efe0035b4c478f7a5a2ae11f730
Pies123dog7 <105671352+Pies123dog7@users.noreply.github.com>
2022-06-14 16:45:58 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+let body = document.body;
+
+function changeRed(){
+    
+    
+    body.style.backgroundColor = "red";
+    body.style.transition = "linear 1s background-color"
+}
+function changeGreen(){
+    body.style.backgroundColor = "green";
+    body.style.transition = "linear 1s background-color"
+    
+}
+function changeBlue(){
+    body.style.backgroundColor = "blue";
+    body.style.transition = "linear 1s background-color"
+    
+}
+function notFocus(){
+    body.style.backgroundColor = "rgb(25, 25, 25)";
+    body.style.transition = "linear 1s background-color"
+}
